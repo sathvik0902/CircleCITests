6d631c55d1636e03173f4bab82368be37aaf16bf
madhu <javamadhuker@gmail.com>
2020-04-03 05:59:42 +0530

    After Adding brower code

--- test_firstcirclecifile.py
+++ test_firstcirclecifile.py
@@ -1,6 +1,10 @@
 import pytest
-
+from selenium import webdriver
 
 def test_sampleDemo():
     print("Hi this is my first circleCI ")
     print("Hi this is my second line circleCI ")
+    driver = webdriver.Chrome(executable_path="D:\\selenium\\drivers\\chromedriver.exe")
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
+    print("URL is OPEN")
