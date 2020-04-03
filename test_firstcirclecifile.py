7096c161e1dc81f125fd868a0c71568272eaaf10
madhu <javamadhuker@gmail.com>
2020-04-03 16:51:45 +0530

    Circleci configuration

--- test_firstcirclecifile.py
+++ test_firstcirclecifile.py
@@ -6,9 +6,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 def test_sampleDemo():
     print("Hi this is my first circleCI ")
     print("Hi this is my second line circleCI ")
-    driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME)
+    driver = webdriver.Chrome(executable_path="D:\\selenium\\drivers\\chromedriver.exe")
+    #driver = webdriver.Remote(
+    #    command_executor='http://127.0.0.1:4444/wd/hub',
+    #    desired_capabilities=DesiredCapabilities.CHROME)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     print("URL is OPEN")
